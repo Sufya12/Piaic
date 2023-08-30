4de6ca4cdc9636441c8fe9b53d1c21b35a0b537e
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-08-30 07:24:13 -0700

    Add files via upload

---
+++ TypeScript/Lecture 1/index.js
@@ -0,0 +1,5 @@
+"use strict";
+//Print The World Hello 
+console.log("Hello World");
+//print The Name 
+console.log("Sufyan Ahmad");
---
+++ TypeScript/Lecture 1/index.ts
@@ -0,0 +1,5 @@
+//Print The World Hello 
+console.log("Hello World");
+
+//print The Name 
+console.log("Sufyan Ahmad");   
