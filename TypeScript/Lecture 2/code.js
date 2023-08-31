021d75e208729d5e28347cdc20c0c57204985e4f
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-08-31 09:20:26 -0700

    Add files via upload

---
+++ TypeScript/Lecture 2/code.js
@@ -0,0 +1,27 @@
+// Create The Variable 
+var a = 52;
+console.log("The Value of A is " + a);
+//Create The Variable 
+var b = 63;
+console.log("The Value Of B is " + b);
+//Addition of Two Variable 
+var addition = a + b;
+console.log("The Addition Of The Two Values " + addition);
+//Subraction Of Two Values
+var Sub = b - a;
+console.log("The Subraction Of The Two Values " + Sub);
+//Multiplation  Of Two Values
+var mul = a * b;
+console.log("The Multiplaction Of The Two Values " + mul);
+//Divided  Of Two Values
+var div = a / b;
+console.log("The Divided Of The Two Values " + div);
+//Power   Of Two Values
+var pow = Math.pow(a, b);
+console.log("The Power Of The Two Values " + pow);
+//Remaider  Of Two Values
+var rem = a % 2;
+console.log("The Remaider Of The Values " + rem);
+//Without Point Divided  Of Two Values
+var withp = a; //b
+console.log("The Without Point Divided  Of The Two Values " + withp);
---
+++ TypeScript/Lecture 2/code.ts
@@ -0,0 +1,39 @@
+// Create The Variable 
+let a =52;
+console.log("The Value of A is "+a);
+
+//Create The Variable 
+const b = 63;
+console.log("The Value Of B is "+b)
+
+
+//Addition of Two Variable 
+let addition = a +b;
+console.log("The Addition Of The Two Values "+addition)
+
+
+//Subraction Of Two Values
+let Sub = b - a
+console.log("The Subraction Of The Two Values "+Sub)
+
+
+//Multiplation  Of Two Values
+let mul = a*b
+console.log("The Multiplaction Of The Two Values "+mul)
+
+
+
+//Divided  Of Two Values
+let div = a/b
+console.log("The Divided Of The Two Values "+div)
+
+
+//Power   Of Two Values
+let pow = a**b
+console.log("The Power Of The Two Values "+pow)
+
+//Remaider  Of Two Values
+let rem = a%2
+console.log("The Remaider Of The Values "+rem)
+
+
