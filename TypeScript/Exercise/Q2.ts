05097f76f5a0d4208cea9c8d9c95edf6f1b96a2c
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-10-28 10:19:18 -0700

    Add files via upload

---
+++ TypeScript/Exercise/Q2.ts
@@ -0,0 +1,10 @@
+/*Personal Message: Store a person’s name in a variable, and print a message to
+that person. Your message should be simple, such as, “Hello Eric, would you like
+to learn some Python today?”
+*/
+//Create the Variable name personName and store the person name
+let personName :string = 'Sufyan Ahmad'
+//Create the Variable nmae message and Some Message
+let message:string=`“Hello ${personName}, would you like to learn some Python today?”`
+//Print the Message
+console.log(message)
