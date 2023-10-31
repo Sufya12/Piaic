77f9a794249d43bb55c0e9f49fc5a584fbf934bc
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-10-31 12:25:15 +0500

    Create q8.ts

---
+++ TypeScript/Exercise/q8.ts
@@ -0,0 +1,15 @@
+let seperator = "_"
+
+console.log(seperator.repeat(80));
+
+console.log(5 + 3);
+console.log(seperator.repeat(80));
+
+console.log(16 - 8);
+console.log(seperator.repeat(80));
+console.log(4 * 2);
+
+console.log(seperator.repeat(80));
+console.log(16 / 2);
+
+console.log(seperator.repeat(80));
