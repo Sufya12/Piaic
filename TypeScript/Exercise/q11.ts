726551a15d21c55ea7f54790c7bd78dcc78a6e42
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-10-31 13:35:50 +0500

    Create q11.ts

---
+++ TypeScript/Exercise/q11.ts
@@ -0,0 +1,11 @@
+let names: string[]=['Sufyan', 'Ali', 'Usman', 'Ahmed'];
+
+console.log(`1st name from list: ${names[0]}`);
+console.log(`2nd name from list: ${names[1]}`);
+console.log(`3rd name from list: ${names[2]}`);
+console.log(`4th name from list: ${names[3]}`);
+
+
+for (let i = 0; i < names.length; i++) 
+ { console.log(`\nWith for loop: At index ${i} name in list is : ${names[i]}`) }
+
