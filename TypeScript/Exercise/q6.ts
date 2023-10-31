d8bf18f1a9a2b102136ac6795d36e15dcef84244
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-10-31 12:19:36 +0500

    Create q6.ts

---
+++ TypeScript/Exercise/q6.ts
@@ -0,0 +1,11 @@
+let personName: string = "\t \t \n\n Sufyan Ahmad \t \n\n";
+
+
+console.log(`Name with whitespace: ${personName}`)
+
+let strippedName: string = personName.trim()
+
+
+
+console.log(`Stripped name: ${strippedName}`);
+
