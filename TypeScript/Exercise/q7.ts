2889351c5e9d1d0cd71b1093ad17fcc6bdbfa9d0
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-10-31 12:22:25 +0500

    Create q7.ts

---
+++ TypeScript/Exercise/q7.ts
@@ -0,0 +1,31 @@
+
+
+let additionResult = 4 + 4;
+
+
+console.log(`Addition Result: ${additionResult}`);
+
+
+// Subtraction 
+
+
+let subtractionResult = 16-8
+
+
+console.log(`Sutracion Results: ${subtractionResult}`);
+
+// Multiplication
+
+let multiplicationResult = 4 * 2;
+
+
+console.log(`Multiplications result: ${multiplicationResult}`); 
+
+// Division
+
+let divisionResult = 16 / 2;
+
+
+
+console.log(`Division Reslt: ${divisionResult}`);
+
